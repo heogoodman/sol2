03395875e014c4186af2d80ab56cb615416ed5ba
heogoodman <tjdwo1030@gmail.com>
2023-10-18 13:05:13 +0900

    sol2

---
+++ MyHomework/src/main/java/com/basic/mykomework/MyHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.basic.mykomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHomeworkApplication.class, args);
+	}
+
+}
